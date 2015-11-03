d9ccec68e17fd581484a18a9dbb98ae8971dac40
mark <mark.vanderloo@gmail.com>
2015-11-03 17:16:48 +0100

    documenting, more functions

---
+++ examples/logging.R
@@ -0,0 +1,23 @@
+# a function that makes some changes.
+change <- function(df){
+  # store the old value
+  .df <- df
+  df[1,1] <- 2*df[1,1]
+  # this is the 'hook' to log, if the function is registered.
+  write_log(old=.df, new=df)
+}
+
+# let's add a logger to the logging registry
+# This logger just writes a single line to screen.
+lg <- add_logger(shortlog())
+
+# let's assign the logger to a function
+set_log(change, lg)
+
+# check the registry
+logreg_status()
+
+w1 <- change(women)
+w2 <- change(women)
+
+
--- pkg/R/logregister.R
+++ pkg/R/logregister.R
@@ -23,6 +23,14 @@ logregister <- setRefClass("logregister"
         message(msg)
         regname
       }
+    , rmlogger = function(logreg){
+        if (is.null(logreg)){
+          rm(list=ls(envir=loggers), pos=loggers)
+        } else {
+          stopifnot( is.character(logreg) )
+          rm(logreg, envir=loggers)
+        }
+    }
     , addfun = function(fun,logreg){
         stopifnot(is.character(fun),is.character(logreg))
         if (!logreg %in% ls(envir=loggers)){
@@ -118,21 +126,165 @@ is_logger <- function(x){
     stop(simpleError(paste(cl,collapse="\n"),call=sys.call(1)[[1]]))
 }
 
-#' Add a logger to the logging registry
+#' Add or remove loggers to the registry
 #'
 #' @param logger  A \code{logger}. By default, \code{\link{shortlog}} is used.
+#' @param logreg \code{[character]}. The name of a registered logger.
+#' 
+#' @section Details:
+#' 
+#' \code{add_logger} adds a logger to the registry. It can henceforth be found
+#' by the name returned by it. An overview of registered loggers is printed by
+#' \code{\link{logreg_status}}.
+#' 
+#' \code{rm_logger} removes a logger from the registry. If the argument \code{logreg} is
+#' not specified, all loggers are removed. Note that possible open connections are not
+#' closed by this command.
+#' 
+#' \code{get_logger} get a reference to a logger object from the registry.
+#' 
+#' \code{get_con} retrieve only the connection field of a logger object (this is what you usually want).
 #'
+#' @section Loggers:
+#' 
+#' The purpose of a \emph{data logger} is to store useful information about changes in the
+#' data without perturbing the flow of data through a script or pipeline. Examples
+#' of loggers that come with the package are
+#' \itemize{
+#' \item{\code{\link{shortlog}} A single-line logger that prints simple
+#' information to the screen (or another connection)}
+#' \item{\code{\link{csvdump}} A logger that dumps a copy of the dataset to csv in a
+#' predefined directory.}
+#' \item{\code{\link{simplediff}} A logger that writes changes to a dataset to a \code{SQLite} database
+#' that can later be queried by the user.}
+#' }
+#' 
+#' 
+#' In this package, a \code{logger} is basically a dressed-up connection. It is a reference class object
+#' that has at least the following field.
+#' \itemize{
+#' \item{\code{con}: A connection or other object that allows the logger to
+#' write and the user to find the logging data.}
+#' }
+#' It must also have the following methods
+#' \itemize{
+#' \item{close()} A function that closes the connection. If the connection does not need to be closed,
+#' (e.g. because it is a directory), it should be a simple function doing nothing.
+#' \item{\code{log(old,new,method,...)}: A function that eats two versions of a data set (old and new)
+#' and a string that tells it what function is calling \code{log}. There is no return value,
+#' but the function writes something to the connection specified by \code{con}. Either of the arguments
+#' may be ignored by \code{log}, but it must at least accept these three arguments, since the
+#' function \code{\link{write_log}} will pass all three.}
+#' \item{\code{status()}: A no-argument function that returns a short string stating the
+#' type of logger and the status of the connection (open, closed)}
+#' }
+#' 
+#' The idea of this package is to be extendable. That is, any package may extend this package
+#' by writing a logger that can be registered by this package. The documentation in this
+#' package is probably not enough to get you started. You can browse the 
+#' \href{https://github.com/data-cleaning/data.log/blob/master/pkg/R/shortlog.R}{source code}
+#' for inspiration of follow the tutorial in the following vignette:
+#' 
+#' \code{  packageVignette("Adding loggers",package="data.log")}
+#' 
+#' Finally, the function \code{\link{logger_template}} creates a file with a template
+#' for you to fill in.
+#' 
+#' 
+#' @return 
+#' \itemize{
+#' \item{\code{add_logger}: \code{[character]}, the name of the logger created in the logregistry.}
+#' \item{\code{rm_logger}: \code{NULL} (invisibly)}
+#' \item{\code{get_logger}: An RC object representing a logger.}
+#' \item{\code{get_con}: The \code{connection} field of a registered logger. 
+#'   The type depends on the nature of the logger.}
+#' }
 #' @export 
 add_logger <- function(logger=shortlog()){
   LOGREG$addlogger(logger)
 }
 
-#' Add a function to the logging registry
+
+#' @rdname  add_logger
+rm_logger <- function(logreg){
+  if (missing(logreg)) logreg <- NULL
+  LOGREG$rmlogger(logreg)
+}
+
+#' @rdname add_logger
+get_logger <- function(logreg){
+  stopifnot(is.character(logreg))
+  LOGREG$loggers[[logreg]]
+}
+
+#' @rdname add_logger
+get_con <- function(logreg){
+  stopifnot(is.character(logreg))
+  LOGREG$logreg[[logreg]]$con
+}
+
+#' Add of remove functions to the registry
 #'
-#' @param fun function to add to registry.
-#' @param logreg \code{[character]} Name of a registered logger.
+#' @param fun \code{[function]} to add or remove.
+#' @param logreg \code{[character]} Name of a registered logger (see details).
+#' 
+#' @section Details:
+#' The central registry maps functions to loggers. The map is in principle n:m.
+#' A single function can be traced by mutliple loggers, and each logger can 
+#' trace one or more functions.
+#' 
+#' \code{set_log(fun,logreg)} hooks a function to a logger. With 
+#' \code{unset_log(fun,logreg)}, a function is unhooked from that logger. If 
+#' \code{logreg} is left unspecified, the function is unhooked from all loggers.
+#' 
+#' \code{logreg_clear()} unhooks all functions from all loggers,
+#' closes all logging connections, and unregisters all loggers.
+#' 
+#' \code{logreg_status()} prints an overview of currently registered loggers
+#' and logged functions.
+#' 
+#' 
+#' @return 
+#' \itemize{
+#' \item{For \code{set_log} \code{[character]}: The name of the logger in the registry (invisibly)}
+#' \item{For \code{unset_log} \code{[character]}: \code{NULL} (invisibly)}
+#' \item{For \code{logreg_clear}\code{[character]}:\code{NULL} (invisibly)}
+#' }
 #' @export
+#' 
+#' @example ../examples/logging.R
+#'
 set_log <- function(fun,logreg){
-  LOGREG$addfun(as.character(substitute(fun)),logreg)
+  invisible(LOGREG$addfun(as.character(substitute(fun)),logreg))
+}
+
+# Remove a function from a logger
+# 
+# @param fun \code{[function]} Function to remove from registry
+# @param logreg \code{[character]} Name of the registered logger that no longer
+#   to be associated with \code{fun}. If this is left empty. the function is
+#   removed completely from the registry so it is not logged by any logger.
+# 
+# 
+
+
+#' @rdname set_log
+unset_log <- function(fun,logreg){
+  fn <- as.character(substitute(fun))
+  logreg <- if (!missing(logreg)) logreg # else NULL
+  invisible(LOGREG$rmfun(fn,logreg))
+}
+
+
+
+#' @rdname set_log
+logreg_clear <- function(){
+  LOGREG$clear()
+}
+
+
+#' @rdname set_log
+logreg_status <- function(){
+  LOGREG$status()
 }
 
